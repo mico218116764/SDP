5d577d43b186fd9fc10867b463fc11b7adf13109
rnatalia <rnatalia.disineyh@gmail.com>
2020-12-09 20:07:47 +0700

    rena

--- Consignment/resources/views/body/barangSaya.blade.php
+++ Consignment/resources/views/body/barangSaya.blade.php
@@ -2,14 +2,15 @@
     .card {
       box-shadow: -10px 10px 10px 10px rgba(0,0,0,0.3);
       transition: 0.3s;
-      width: 98%;
+      width: 100%;
       padding: 2%;
       padding-right: 5%;
       /* border: 1px solid gray; */
       border-radius: 1%;
+      margin-bottom: 2%;
     }
     .container {
-      padding: 2px 16px;
+      padding: 1% 1% 1% 1%;
     }
     .containerS{
         margin: 1%;
@@ -20,22 +21,19 @@
         height: 100%;
         background-repeat:no-repeat;
     }
-    .login-form{
-        margin-top: -2%;
-    }
 </style>
 <body>
-<div class="login-form containerS" style="height: 100px">
+<div class="login-form containerS" style="height: 50%">
   {{--<img src=" {{asset('images/background1.jpg')}}" alt="" style="margin-top: -3%; margin-left: -2%;">--}} 
     @if (count($daftarKatalog) == 0)
-        <div class="card" style="height: 10%">
+        <div class="card" style="height: 50%">
     @else
         <div class="card">
     @endif
         <h1><b>Barang saya</b></h1><br>
         @foreach ($daftarKatalog as $item)
         <div class="row">
-            <div class="col-sm-4">
+            <div class="col-md-3">
                 <a href="{{url('/detailbarangku/'.$item->PENGAJUAN_ID)}}">
                 <div class="panel panel-primary" >
                     <div class="panel-heading">
@@ -47,7 +45,7 @@
                         @endif
 
                     </div>
-                    <div class="panel-body" style="width: 250px;height: 250px; margin-left: auto; margin-right: auto;">
+                    <div class="panel-body" style="width: 100%; height: 100%; ">
                     <img src="{{asset('images/'.$item->FOTO_KIRI)}}" class="img-responsive"alt="Image">
                 </div>
                 <div class="panel-footer desc">
--- Consignment/resources/views/body/statusbarang.blade.php
+++ Consignment/resources/views/body/statusbarang.blade.php
@@ -22,7 +22,7 @@
     <div class="card">
         <form method="POST" action="{{url('/addBank')}}">
             @csrf
-            <div class="container" style="margin-top:2%; padding:1%;">
+            <div class="container">
                 <div class="form-group">
 
                 </div>
@@ -36,59 +36,55 @@
         <form action="{{"/cekPengiriman"}}" method="get">
             @csrf
             <div class="login-form">
-                <div style="margin-top:2%; padding:1%;">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th style="text-align:center">Transaksi ID</th>
-                                <th style="text-align:center">Nama Barang</th>
-                                <th style="text-align:center">Status Barang</th>
-                                <th style="text-align:center">Alasan</th>
-                                <th style="text-align:center">Action</th>
-                            </tr>
-                        </thead>
-                        @foreach ($daftarTransaksi as $transaksi)
-                            <tbody>
-                                <th style="text-align:center">{{$transaksi->transaksi_id}}</th>
-                                @foreach ($daftarPengajuan as $pengajuan)
-                                    @if ($transaksi->PENGAJUAN_ID == $pengajuan->PENGAJUAN_ID)
-                                        <th style="text-align:center">{{$pengajuan->NAMA_BARANG}}</th>
-                                    @endif
-                                @endforeach
-                                @if($transaksi->status == 0)
-                                    <th style="text-align:center">Barang masih di proses</th>
-                                @elseif ($transaksi->status == 1)
-                                    <th style="text-align:center">Menunggu seller mengirim resi</th>
-                                @elseif($transaksi->status == 2)
-                                    <th style="text-align:center">Rejected</th>
-                                @elseif($transaksi->status == 4)
-                                    <th style="text-align:center">Barang Telah dikirim</th>
-                                @elseif($transaksi->status == 5)
-                                    <th style="text-align:center">Transaksi Selesai</th>
-                                @else
-                                    <th style="text-align:center">Out Of Time</th>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th style="text-align:center">Transaksi ID</th>
+                            <th style="text-align:center">Nama Barang</th>
+                            <th style="text-align:center">Status Barang</th>
+                            <th style="text-align:center">Alasan</th>
+                            <th style="text-align:center">Action</th>
+                        </tr>
+                    </thead>
+                    @foreach ($daftarTransaksi as $transaksi)
+                        <tbody>
+                            <th style="text-align:center">{{$transaksi->transaksi_id}}</th>
+                            @foreach ($daftarPengajuan as $pengajuan)
+                                @if ($transaksi->PENGAJUAN_ID == $pengajuan->PENGAJUAN_ID)
+                                    <th style="text-align:center">{{$pengajuan->NAMA_BARANG}}</th>
                                 @endif
-
-                                @if ($transaksi->alasan != null)
-                                    <th style="text-align:center">{{$transaksi->alasan}}</th>
-                                @else
-                                    <th style="text-align:center">-</th>
+                            @endforeach
+                            @if($transaksi->status == 0)
+                                <th style="text-align:center">Barang masih di proses</th>
+                            @elseif ($transaksi->status == 1)
+                                <th style="text-align:center">Menunggu seller mengirim resi</th>
+                            @elseif($transaksi->status == 2)
+                                <th style="text-align:center">Rejected</th>
+                            @elseif($transaksi->status == 4)
+                                <th style="text-align:center">Barang Telah dikirim</th>
+                            @elseif($transaksi->status == 5)
+                                <th style="text-align:center">Transaksi Selesai</th>
+                            @else
+                                <th style="text-align:center">Out Of Time</th>
+                            @endif
+                            @if ($transaksi->alasan != null)
+                                <th style="text-align:center">{{$transaksi->alasan}}</th>
+                            @else
+                                <th style="text-align:center">-</th>
+                            @endif
+                            @if ($transaksi->status == 1 || $transaksi->status == 4)
+                                <th style="text-align:center"><button type="submit" name="butSub" value="{{$transaksi->transaksi_id}}">Check Status</button></th>
+                            @elseif ($transaksi->status == 0)
+                                <th style="text-align:center" ><button value="{{$transaksi->transaksi_id}} " name="butSub" type="submit">Check Status</button></th>
+                            @elseif ($transaksi->status == 5)
+                                <th style="text-align:center" >Done</th>
+                            @else
+                                <th style="text-align:center">Please Apply again</th>
                                 @endif
-                                @if ($transaksi->status == 1 || $transaksi->status == 4)
-                                    <th style="text-align:center"><button type="submit" name="butSub" value="{{$transaksi->transaksi_id}}">Check Status</button></th>
-                                @elseif ($transaksi->status == 0)
-                                    <th style="text-align:center" ><button value="{{$transaksi->transaksi_id}} " name="butSub" type="submit">Check Status</button></th>
-                                @elseif ($transaksi->status == 5)
-                                    <th style="text-align:center" >Done</th>
-                                @else
-                                    <th style="text-align:center">Please Apply again</th>
-                                @endif
-
-                            </tbody>
-                        @endforeach
-                    </table>
+                        </tbody>
+                    @endforeach
+                </table>
                 </div>
-            </div>
         </form>
     </div>
     </div>
