c69f1872508f02f810e47f494d9c9ed4059bad36
mico218116764 <72438982+mico218116764@users.noreply.github.com>
2020-12-14 13:40:57 +0700

    ming

--- Consignment/resources/views/body/daftarTransaksi.blade.php
+++ Consignment/resources/views/body/daftarTransaksi.blade.php
@@ -3,7 +3,7 @@
         text-align: center;
     }
     body{
-        background-image: url("{{asset('images/background1.jpg')}}");
+        /* background-image: url("{{asset('images/background1.jpg')}}"); */
         width: 100%;
         height: 100%;
         background-repeat:no-repeat;
@@ -26,39 +26,43 @@
     }
 </style>
 <body>
-    <div class="addAdmin-form">
+    {{-- <div class="addAdmin-form">
     <div class="card">
+        <h1></h1> --}}
     {{--<img src=" {{asset('images/background1.jpg')}}" alt="" style="margin-top: -5%">--}}
-        <br><br>
+        {{-- <br><br>
         <style>
             td {
                 text-align: center;
             }
         </style>​
-        <div class="login-form">
-            <div style="padding-left: 200px;padding-right: 200px;">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th style="text-align:center">Transaksi ID</th>
-                            <th style="text-align:center">Pengajuan ID</th>
-                            <th style="text-align:center">User Pengaju</th>
-                            <th style="text-align:center">Action</th>
-                        </tr>
-                    </thead>
-                    @foreach ($daftarTransaksi as $transaksi)
-                        <tbody>
-                            <th style="text-align:center">{{$transaksi->transaksi_id}}</th>
-                            <th style="text-align:center">{{$transaksi->PENGAJUAN_ID}}</th>
-                            <th style="text-align:center">{{$transaksi->email_pembeli}}</th>
-                            <th style="text-align:center"><a href="{{url('detailTransaksi/'.$transaksi->transaksi_id)}}"><button type="submit">Check</button></a></th>
-                        </tbody>
-                    @endforeach
-                </table>
-            </div>
+
+    </div>
+    </div> --}}
+    {{-- <div class="login-form">
+        <h1>Test</h1>
+        <div style="padding-left: 200px;padding-right: 200px;">
+
         </div>
-    </div>
-    </div>
+    </div> --}}
+    <table class="table">
+        <thead>
+            <tr>
+                <th style="text-align:center">Transaksi ID</th>
+                <th style="text-align:center">Pengajuan ID</th>
+                <th style="text-align:center">User Pengaju</th>
+                <th style="text-align:center">Action</th>
+            </tr>
+        </thead>
+        @foreach ($daftarTransaksi as $transaksi)
+            <tbody>
+                <th style="text-align:center">{{$transaksi->transaksi_id}}</th>
+                <th style="text-align:center">{{$transaksi->PENGAJUAN_ID}}</th>
+                <th style="text-align:center">{{$transaksi->email_pembeli}}</th>
+                <th style="text-align:center"><a href="{{url('detailTransaksi/'.$transaksi->transaksi_id)}}"><button type="submit">Check</button></a></th>
+            </tbody>
+        @endforeach
+    </table>
 </body>
 @if (session('alert'))
     <div class="alert alert-success">
